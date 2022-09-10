0c80ded8bc1d51ae08dc3f518fa39e2f559d59d5
Travis Okonicha <okonicha.ugo@gmail.com>
2022-09-10 11:37:15 +0100

    final.

---
+++ lib_login_flow/main.dart
@@ -0,0 +1,14 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+
+import 'app.dart';
+
+void main() {
+  runApp(
+    App(
+      authenticationRepository: AuthenticationRepository(),
+      userRepository: UserRepository(),
+    ),
+  );
+}
